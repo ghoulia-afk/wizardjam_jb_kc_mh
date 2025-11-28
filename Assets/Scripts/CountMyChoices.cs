7212a85da9d4da4b87ffd549a3a531763a1ecc6d
ghoulia-afk <kathleen.clark@mymail.champlain.edu>
2025-11-28 15:27:52 -0500

    Temporary CountMyChoicesScript + Commenting out some var declarations

---
+++ Assets/Scripts/CountMyChoices.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountMyChoices : MonoBehaviour
+ {
+        //A choice counter that runs when called
+   /* public DialogueRunner dialogueRunner;
+
+    // [YarnCommand("sir_swapsalot")]
+
+    public void Awake()
+    {
+        dialogueRunner.AddCommandHandler<string>(
+            "countup_together",     // the name of the command
+            Together_End // the method to run
+        );
+
+        dialogueRunner.AddCommandHandler<string>(
+            "countup_alone",
+            Alone_End
+        );
+    }
+    public void Scenecalibur(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        Debug.Log("Loaded scene" + (sceneName));
+    }
+
+    public void Excalamune(string sceneName)
+    {
+        SceneManager.UnloadSceneAsync(sceneName); //may need to change to UnloadScene if there are issues - KGC
+        Debug.Log("Unloaded scene" + sceneName);
+    } */
+} 
