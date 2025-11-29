65e8020b55e46ae7725eb5aa99956d2202a8292c
alexis <alexis.morin.mail@gmail.com>
2025-11-29 16:28:56 -0500

    Scene loader jank

--- Assets/Scripts/SwapMyScenes.cs
+++ Assets/Scripts/SwapMyScenes.cs
@@ -6,9 +6,19 @@ using Yarn.Unity;
 
 public class SwapMyScenes : MonoBehaviour
 { //A scene swapper that runs whenever sir_swapsalot is called 
+    
+    public string defaultScene;
+    
     public DialogueRunner dialogueRunner;
 
+    public LevelTransitionFade fade;
+
     // [YarnCommand("sir_swapsalot")]
+
+public void Start(){
+     SceneManager.LoadScene (defaultScene, LoadSceneMode.Additive);
+     SceneManager.SetActiveScene(SceneManager.GetSceneByName(defaultScene));
+}
 
     public void Awake()
     {
@@ -24,13 +34,33 @@ public class SwapMyScenes : MonoBehaviour
     }
     public void Scenecalibur (string sceneName)
     {
-        SceneManager.LoadScene (sceneName, LoadSceneMode.Additive);
-        Debug.Log("Loaded scene" + (sceneName));
+        StartCoroutine(LoadSceneTime(sceneName));
+
+       
     }
+
+    public IEnumerator LoadSceneTime(string sceneName){
+    fade.Fade();
+    yield return new WaitForSeconds(0.2f);
+    SceneManager.LoadScene (sceneName, LoadSceneMode.Additive);
+    SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
+    Debug.Log("Loaded scene" + (sceneName));
+    }
+
+    public IEnumerator UnloadSceneTime(string sceneName){
+    fade.Fade();
+    yield return new WaitForSeconds(0.2f);
+    SceneManager.UnloadSceneAsync(sceneName); //may need to change to UnloadScene if there are issues - KGC
+    Debug.Log("Unloaded scene" + sceneName);
+    }
+
 
     public void Excalamune(string sceneName)
     {
-        SceneManager.UnloadSceneAsync(sceneName); //may need to change to UnloadScene if there are issues - KGC
-        Debug.Log("Unloaded scene" + sceneName);
+ StartCoroutine(UnloadSceneTime(sceneName));
+
+        
     }
+
+
 }
