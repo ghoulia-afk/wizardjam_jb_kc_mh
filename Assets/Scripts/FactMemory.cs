3501ee6c5a6ff130a69de55fce26db5309974cd9
ghoulia-afk <kathleen.clark@mymail.champlain.edu>
2025-11-29 12:54:34 -0500

    Update FactMemory.cs
    Adding functionality to check if the player has gotten the Together or Alone Endings

--- Assets/Scripts/FactMemory.cs
+++ Assets/Scripts/FactMemory.cs
@@ -17,11 +17,70 @@ public class FactMemory : Yarn.Unity.VariableStorageBehaviour
 
     public static List<Fact> factList = new List<Fact>();
 
+    //Code via Alexis Morin
+    //integers that keep track of what ending the player is "working towards" - KGC
+    static int togetherEndingCounter = 0;
+    static int aloneEndingCounter = 0;
+
+    public static void AddTogetherEndingCounter() //function to add to counter for Together Ending - KGC
+    {
+        togetherEndingCounter += 1;
+    }
+
+    public static void AddAloneEndingCounter() //function to add to counter for Alone Ending - KGC
+    { 
+        aloneEndingCounter += 1; 
+    }
+
+    public static bool TryTogetherEndingCounter(int testCount) //function that Yarn can call to see what ending the player has gotten
+    {
+        if (togetherEndingCounter >= testCount)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static bool TryAloneEndingCounter(int testCount) //function that Yarn can call to see what ending the player has gotten
+    {
+        if (aloneEndingCounter >= testCount)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static bool CheckTogetherEnding() //function that Yarn can call to see what ending the player has gotten
+    {
+        if (togetherEndingCounter >= aloneEndingCounter)
+        {
+            return true; //this means the player has gotten the together ending
+        }
+        else
+        {
+            return false; //this means the player has gotten the alone ending
+        }
+    }
+
 
     public void Awake()
     {
         dialogueRunner.AddCommandHandler<string>("AddFact", AddFact);
         dialogueRunner.AddFunction<string, bool>("TryFact", TryFact);
+
+        dialogueRunner.AddFunction<bool>("CheckTogetherEnding", CheckTogetherEnding);
+
+        dialogueRunner.AddCommandHandler("AddTogetherEndingCounter", AddTogetherEndingCounter);
+        dialogueRunner.AddCommandHandler("AddAloneEndingCounter", AddAloneEndingCounter);
+
+        dialogueRunner.AddFunction<int, bool>("TryTogetherEndingCounter", TryTogetherEndingCounter);
+        dialogueRunner.AddFunction<int, bool>("TryAloneEndingCounter", TryAloneEndingCounter);
     }
 
     public void ClearFacts()
