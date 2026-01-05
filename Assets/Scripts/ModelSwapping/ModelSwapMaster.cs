393c1c8ddbfe2dc5c0bd798513c75fd3682861ea
jadyboi1 <jadin.brown@mymail.champlain.edu>
2026-01-04 19:48:30 -0500

    FIXED KING MESHER

--- Assets/Scripts/ModelSwapping/ModelSwapMaster.cs
+++ Assets/Scripts/ModelSwapping/ModelSwapMaster.cs
@@ -10,9 +10,7 @@ public class ModelSwapMaster : MonoBehaviour
     public List<string> bodyTags;
     public List<string> headTags;
 
-    private GameObject currentBody;
-
-    private static List<ModelSwapMaster> swappers;
+    public static List<ModelSwapMaster> swappers = new List<ModelSwapMaster>();
 
     private void Awake()
     {
@@ -22,23 +20,79 @@ public class ModelSwapMaster : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < bodies.Count; i++) bodies[i].gameObject.SetActive(false);
+        //foreach (var b in bodies) b.SetActive(false);
     }
 
     /// <summary>
-    /// NOTE: MUST BE INITIATED AT THE START OF THE SCENE FOR BOTH THE HEAD AND THE BODY!
     /// Switches between different bodies or heads for the two characters.
     /// </summary>
     /// <param name="tag">Denotes which model to switch to according to the 'tags' list in each object with a ModelSwapMaster component.
-    /// Bodies can be swapped independently of the head, but you only need to give one arguement to do either since only one of the two swapper
+    /// Bodies can be swapped independently of the head and vice versa using either "(name)_body_..." or "(name)_head_...".
     /// types will pick it up.</param>
     public static void StaticSwapModels(string tag)
     {
-        for (int i = 0; i < swappers.Count; i++)
-        {
-            if (swappers[i] == null) swappers.RemoveAt(i);
-            else { swappers[i].SwapBodies(tag); swappers[i].SwapHeads(tag); }
-        }
+        Debug.Log($"Attempting method StaticSwapModels('{tag}')...");
+
+        List<int> nullIndices = new List<int>();
+        for (int i = 0; i < swappers.Count; i++) if (swappers[i] == null) nullIndices.Add(i);
+        swappers = RemoveNullIndices(swappers, nullIndices);
+
+        Debug.Log($"Current swapper count: {swappers.Count}");
+
+        foreach (var swapper in swappers) { swapper.SwapBodies(tag); swapper.SwapHeads(tag); } 
+
+        //else { swappers[i].SwapBodies(tag); swappers[i].SwapHeads(tag); }
+    }
+
+    [ContextMenu("Test List Indices")]
+    public void TestLists()
+    {
+        List<int> mainList = new List<int>
+        { 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, };
+
+        string output = string.Empty;
+        foreach (int i in mainList) output += ($"{i}, ");
+        Debug.Log($"mainList: {output}");
+
+        ////
+
+        List<int> removeIndices = new List<int>
+        { 2, 4, 3, 1, };
+
+        output = string.Empty;
+        foreach (int i in removeIndices) output += ($"{i}, ");
+        Debug.Log($"removeIndices: {output}");
+
+        ////
+
+        removeIndices.Sort();
+        removeIndices.Reverse();
+
+        output = string.Empty;
+        foreach (int i in removeIndices) output += ($"{i}, ");
+        Debug.Log($"removeIndices (sorted and reversed): {output}");
+
+        ////
+
+        foreach (int i in removeIndices) mainList.RemoveAt(i);
+
+        output = string.Empty;
+        foreach (int i in mainList) output += ($"{i}, ");
+        Debug.Log($"mainList (removed indices): {output}");
+    }
+
+    /// <summary>
+    /// Removes the specified indices from a list. (ModelSwapMaster)
+    /// </summary>
+    /// <param name="list">The initial list.</param>
+    /// <param name="indices">The indices you'd like to remove from the initial list.</param>
+    /// <returns></returns>
+    private static List<ModelSwapMaster> RemoveNullIndices(List<ModelSwapMaster> list, List<int> indices)
+    {
+        indices.Sort();
+        indices.Reverse();
+        foreach (int i in indices) list.RemoveAt(i);
+        return list;
     }
 
     public void SwapBodies(string tag) // WHEN BODIES ACTIVATE, THEIR HEADS ACTIVATE AS WELL!
@@ -59,9 +113,10 @@ public class ModelSwapMaster : MonoBehaviour
             return;
         }
 
-        if (currentBody != null) currentBody.gameObject.SetActive(false);
-        currentBody = bodies[requestIndex];
-        currentBody.gameObject.SetActive(true);
+        foreach (var body in bodies) body.SetActive(false);
+        bodies[requestIndex].SetActive(true);
+
+        Debug.Log($"Successfully swapped BODIES for tag: '{tag}'");
     }
 
     public void SwapHeads(string tag)
@@ -74,7 +129,9 @@ public class ModelSwapMaster : MonoBehaviour
 
         if (!headTags.Contains(tag)) return; // Request was prolly meant for a different swapper, or the body
 
-        foreach (GameObject head in heads) head.transform.localEulerAngles = GetRotation(tag);
+        foreach (var head in heads) head.transform.localEulerAngles = GetRotation(tag);
+
+        Debug.Log($"Successfully swapped HEADS for tag: '{tag}'");
     }
 
     private Vector3 GetRotation(string tag)
